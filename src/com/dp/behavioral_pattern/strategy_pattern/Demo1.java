0db7b7488fedd33d527b6e5da55093f0f2deb46f
Sathish-Kumar-E <sathishkumar13121998@gmail.com>
2025-05-11 23:42:59 -0400

    Strategy Pattern, Demo 1

---
+++ src/com/dp/behavioral_pattern/strategy_pattern/Demo1.java
@@ -0,0 +1,56 @@
+package com.dp.behavioral_pattern.strategy_pattern;
+
+
+// Strategy
+interface PaymentStrategy{
+	void processPayment();
+}
+
+
+// Concrete Strategy : For credit card payment
+class CreditCardPayment implements PaymentStrategy {
+	@Override
+	public void processPayment() {
+		System.out.println("Making payment via Credit Card");
+	}
+}
+
+
+// Concrete Strategy : For debit card payment
+class DebitCardPayment implements PaymentStrategy {
+	@Override
+	public void processPayment() {
+		System.out.println("Making payment via Debit Card");	
+	}
+}
+
+
+// Concrete Strategy : For UPI payment
+class UPIPayment implements PaymentStrategy {
+	@Override
+	public void processPayment() {
+		System.out.println("Making payment via UPI");
+	}
+}
+
+
+class PaymentService{
+	private PaymentStrategy strategy;
+	
+	public void setPaymentStrategy(PaymentStrategy strategy) {
+		this.strategy = strategy;
+	}
+	
+	public void pay() {
+		strategy.processPayment();
+	}
+}
+
+
+public class Demo1 {
+	public static void main(String[] args) {
+		PaymentService paymentService = new PaymentService();
+		paymentService.setPaymentStrategy(new CreditCardPayment());
+		paymentService.pay();
+	}
+}
